e65d8834a79ab30ca525fac9d2589294e72bd704
RajdeepSarkar2000 <rajdeepsarkar999@gmail.com>
2023-03-31 01:12:03 +0530

    Initial design

---
+++ sections/About.jsx
@@ -0,0 +1,17 @@
+'use client';
+
+import {motion} from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '../styles';
+import {fadeIn, staggerContainer,} from '../utils/motion'
+
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className ="gardient-02 z-0">
+    About section</div>
+  </section>
+);
+
+export default About;
